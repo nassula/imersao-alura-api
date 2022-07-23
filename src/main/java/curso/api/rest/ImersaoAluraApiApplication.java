affb00c77d3d445c2fd89279c46b831e90655e98
Diego Nassula <diegonassula@gmail.com>
2022-07-23 10:59:42 -0300

    Commit inicial do projeto

---
+++ src/main/java/curso/api/rest/ImersaoAluraApiApplication.java
@@ -0,0 +1,14 @@
+package curso.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoAluraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoAluraApiApplication.class, args);
+		System.out.println("Olá mundo");
+	}
+
+}
